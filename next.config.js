66a50426ae6148a527aeb8a57374c4abb297cd99
unknown <luk.vainius@gmail.com>
2023-06-11 21:28:29 +0100

    removed cssloader postcss configs from next.config.js

--- next.config.js
+++ next.config.js
@@ -6,34 +6,6 @@ const nextConfig = {
 	images: {
 		domains: ["static.foxbusiness.com", "a57.foxnews.com"],
 	},
-	reactStrictMode: true,
-	webpack: (config, { defaultLoaders }) => {
-		config.module.rules.push({
-			test: /\.css$/,
-			use: [
-				defaultLoaders.babel,
-				{
-					loader: require.resolve("css-loader"),
-					options: {
-						importLoaders: 1,
-						modules: {
-							auto: true,
-						},
-					},
-				},
-				{
-					loader: require.resolve("postcss-loader"),
-					options: {
-						postcssOptions: {
-							plugins: ["tailwindcss", "autoprefixer"],
-						},
-					},
-				},
-			],
-		});
-
-		return config;
-	},
 };
 
 module.exports = nextConfig;
