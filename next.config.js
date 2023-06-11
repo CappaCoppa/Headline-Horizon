5faf3fbb17f6d5014cf0b792857e6227831127f7
unknown <luk.vainius@gmail.com>
2023-06-11 20:59:33 +0100

    updating next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,39 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-  images: {
-    domains: ["static.foxbusiness.com", "a57.foxnews.com"],
-  },
+	experimental: {
+		appDir: true,
+	},
+	images: {
+		domains: ["static.foxbusiness.com", "a57.foxnews.com"],
+	},
+	reactStrictMode: true,
+	webpack: (config, { defaultLoaders }) => {
+		config.module.rules.push({
+			test: /\.css$/,
+			use: [
+				defaultLoaders.babel,
+				{
+					loader: require.resolve("css-loader"),
+					options: {
+						importLoaders: 1,
+						modules: {
+							auto: true,
+						},
+					},
+				},
+				{
+					loader: require.resolve("postcss-loader"),
+					options: {
+						postcssOptions: {
+							plugins: ["tailwindcss", "autoprefixer"],
+						},
+					},
+				},
+			],
+		});
+
+		return config;
+	},
 };
 
 module.exports = nextConfig;
