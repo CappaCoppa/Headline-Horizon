ec4b89e6ee78a502668268f2ad36a7ffde0c5f3e
unknown <luk.vainius@gmail.com>
2023-06-18 15:06:35 +0100

    working on .json() returning undefined

--- src/app/api/articles/route.js
+++ src/app/api/articles/route.js
@@ -9,6 +9,7 @@ export async function GET(req) {
 	return new Response(response, {
 		status: 200,
 		headers: {
+			"Content-Type": "application/json",
 			"Access-Control-Allow-Origin": "*",
 			"Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
 			"Access-Control-Allow-Headers": "Content-Type, Authorization",
