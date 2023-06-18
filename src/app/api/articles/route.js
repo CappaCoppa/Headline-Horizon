5c5f26ef67013db6db4e965d9ed8969db4e4a89d
unknown <luk.vainius@gmail.com>
2023-06-18 02:24:03 +0100

    working on api side

--- src/app/api/articles/route.js
+++ src/app/api/articles/route.js
@@ -1,19 +1,17 @@
-import cors from "cors";
 import db_connection from "@/utils/db/connection";
 
 export async function GET(req) {
-	return cors(async () => {
-		const client = await db_connection();
-		const db = await client.db("headline_horrizon");
-		const collection = await db.collection("articles");
-		const all_articles = await collection.find().toArray();
-		return new Response(JSON.stringify(all_articles), {
-			status: 200,
-			headers: {
-				"Access-Control-Allow-Origin": "*",
-				"Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-				"Access-Control-Allow-Headers": "Content-Type, Authorization",
-			},
-		});
+	const client = await db_connection();
+	const db = await client.db("headline_horrizon");
+	const collection = await db.collection("articles");
+	const all_articles = await collection.find().toArray();
+	const response = JSON.stringify(all_articles);
+	return new Response(response, {
+		status: 200,
+		headers: {
+			"Access-Control-Allow-Origin": "*",
+			"Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+			"Access-Control-Allow-Headers": "Content-Type, Authorization",
+		},
 	});
 }
