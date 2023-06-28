90a13da638dedd6a89561aec446c236657942ef5
unknown <luk.vainius@gmail.com>
2023-06-28 22:57:05 +0100

    fixing all the stuff

--- src/app/[category]/[sub_category]/[article]/page.js
+++ src/app/[category]/[sub_category]/[article]/page.js
@@ -1,94 +1,94 @@
-// import React from "react";
-// import Image from "next/image";
+import React from "react";
+import Image from "next/image";
 
-// const getData = async (sub_category, id) => {
-// 	try {
-// 		const res = await fetch(
-// 			`${process.env.API_URL}/articles/${sub_category}/${id}`,
-// 			{ cache: "no-cache" }
-// 		);
-// 		return await res.json();
-// 	} catch (err) {
-// 		console.log(`An error occured while fetching data from the server: ${err}`);
-// 	}
-// };
+const getData = async (sub_category, id) => {
+	try {
+		const res = await fetch(
+			`${process.env.API_URL}/articles/${sub_category}/${id}`,
+			{ cache: "no-cache" }
+		);
+		return await res.json();
+	} catch (err) {
+		console.log(`An error occured while fetching data from the server: ${err}`);
+	}
+};
 
-// export default async function Article({ params }) {
-// 	const articleObject = await getData(params.sub_category, params.article);
-// 	const numSentences = articleObject.article.length;
-// 	const numImages = articleObject.images.length;
-// 	const sentencesPerImage = Math.ceil(numSentences / numImages);
+export default async function Article({ params }) {
+	const articleObject = await getData(params.sub_category, params.article);
+	const numSentences = articleObject.article.length;
+	const numImages = articleObject.images.length;
+	const sentencesPerImage = Math.ceil(numSentences / numImages);
 
-// 	const mappingContent = () =>
-// 		articleObject.article.map((sentence, index) => {
-// 			const shouldDisplayImage = index % sentencesPerImage === 0 || index === 0;
-// 			const imageIndex = Math.min(
-// 				Math.floor(index / sentencesPerImage),
-// 				numImages - 1
-// 			);
+	const mappingContent = () =>
+		articleObject.article.map((sentence, index) => {
+			const shouldDisplayImage = index % sentencesPerImage === 0 || index === 0;
+			const imageIndex = Math.min(
+				Math.floor(index / sentencesPerImage),
+				numImages - 1
+			);
 
-// 			return (
-// 				<React.Fragment key={index}>
-// 					{shouldDisplayImage && (
-// 						<div className="display flex flex-col">
-// 							<div className="h-[700px] relative">
-// 								<Image
-// 									className="align-middle h-auto"
-// 									fill
-// 									src={articleObject.images[imageIndex].image_url}
-// 									alt={articleObject.images[imageIndex].image_alt}
-// 								/>
-// 							</div>
-// 							{index === 0 && (
-// 								<audio
-// 									controls
-// 									className="w-full my-16 border-b-black-10 border-b-2">
-// 									<source
-// 										src={articleObject.audio_link}
-// 										className="bg-primary"
-// 										type="audio/mpeg"
-// 									/>
-// 								</audio>
-// 							)}
-// 						</div>
-// 					)}
-// 					<h6 className="text-black text-left py-16">{sentence}</h6>
-// 				</React.Fragment>
-// 			);
-// 		});
+			return (
+				<React.Fragment key={index}>
+					{shouldDisplayImage && (
+						<div className="display flex flex-col">
+							<div className="h-[700px] relative">
+								<Image
+									className="align-middle h-auto"
+									fill
+									src={articleObject.images[imageIndex].image_url}
+									alt={articleObject.images[imageIndex].image_alt}
+								/>
+							</div>
+							{index === 0 && (
+								<audio
+									controls
+									className="w-full my-16 border-b-black-10 border-b-2">
+									<source
+										src={articleObject.audio_link}
+										className="bg-primary"
+										type="audio/mpeg"
+									/>
+								</audio>
+							)}
+						</div>
+					)}
+					<h6 className="text-black text-left py-16">{sentence}</h6>
+				</React.Fragment>
+			);
+		});
 
-// 	return (
-// 		<main className="bg-black-5 px-64 py-32">
-// 			<div className="items-center align-middle flex justify-center gap-8 pb-8">
-// 				<h7 className="font-bold text-primary uppercase">
-// 					{articleObject.sub_category}
-// 				</h7>
-// 				<div className="rounded-full bg-black-25 w-[4px] h-[4px]"></div>
-// 				<h7>
-// 					<span className="font-bold uppercase">published: </span>{" "}
-// 					{articleObject.date}
-// 				</h7>
-// 			</div>
-// 			<h1 className="px-128 text-black font-antic uppercase text-center font-bold pb-16">
-// 				{articleObject.headline}
-// 			</h1>
-// 			<h5 className="px-256 pb-16 text-black text-center">
-// 				{articleObject.sub_headline}
-// 			</h5>
-// 			<div className="flex border-y-black-10 border-y-2 py-16 gap-16">
-// 				<div className="w-3/4">{mappingContent()}</div>
-// 				<div className="border-l-2 border-black-10 pl-8 w-1/4">
-// 					<div className="bg-secondary h-[550px] w-[100%] text-center flex justify-center items-center">
-// 						<h6>Add container</h6>
-// 					</div>
-// 					<div className="py-16 sticky top-0">
-// 						<h6 className="text-black-5 bg-primary  font-antic text-left p-8 uppercase">
-// 							Related Articles
-// 						</h6>
-// 						{/* {mappingRelatedArticles()} */}
-// 					</div>
-// 				</div>
-// 			</div>
-// 		</main>
-// 	);
-// }
+	return (
+		<main className="bg-black-5 px-64 py-32">
+			<div className="items-center align-middle flex justify-center gap-8 pb-8">
+				<h7 className="font-bold text-primary uppercase">
+					{articleObject.sub_category}
+				</h7>
+				<div className="rounded-full bg-black-25 w-[4px] h-[4px]"></div>
+				<h7>
+					<span className="font-bold uppercase">published: </span>{" "}
+					{articleObject.date}
+				</h7>
+			</div>
+			<h1 className="px-128 text-black font-antic uppercase text-center font-bold pb-16">
+				{articleObject.headline}
+			</h1>
+			<h5 className="px-256 pb-16 text-black text-center">
+				{articleObject.sub_headline}
+			</h5>
+			<div className="flex border-y-black-10 border-y-2 py-16 gap-16">
+				<div className="w-3/4">{mappingContent()}</div>
+				<div className="border-l-2 border-black-10 pl-8 w-1/4">
+					<div className="bg-secondary h-[550px] w-[100%] text-center flex justify-center items-center">
+						<h6>Add container</h6>
+					</div>
+					<div className="py-16 sticky top-0">
+						<h6 className="text-black-5 bg-primary  font-antic text-left p-8 uppercase">
+							Related Articles
+						</h6>
+						{/* {mappingRelatedArticles()} */}
+					</div>
+				</div>
+			</div>
+		</main>
+	);
+}
--- src/app/[category]/[sub_category]/page.js
+++ src/app/[category]/[sub_category]/page.js
@@ -1,67 +1,67 @@
-// import Image from "next/image";
-// import Link from "next/link";
+import Image from "next/image";
+import Link from "next/link";
 
-// const getData = async (sub_category) => {
-// 	try {
-// 		const res = await fetch(`${process.env.API_URL}/articles/${sub_category}`, {
-// 			next: { revalidate: 120 },
-// 		});
-// 		return await res.json();
-// 	} catch (err) {
-// 		console.log(`An error occured while fetching data from the server: ${err}`);
-// 	}
-// };
+const getData = async (sub_category) => {
+	try {
+		const res = await fetch(`${process.env.API_URL}/articles/${sub_category}`, {
+			next: { revalidate: 120 },
+		});
+		return await res.json();
+	} catch (err) {
+		console.log(`An error occured while fetching data from the server: ${err}`);
+	}
+};
 
-// const mappingSubCategoryArticles = (array) => {
-// 	return array.map((article, index) => {
-// 		return (
-// 			<Link
-// 				href={`/${article.category}/${article.sub_category}/${article._id}`}
-// 				key={index}>
-// 				<div className="flex flex-row gap-16 hover:bg-black-10 transition-all ease-in-out cursor-pointer">
-// 					<div className="relative w-1/4 h-[170px]">
-// 						<Image
-// 							src={article.images[0].image_url}
-// 							alt={article.images[0].image_alt}
-// 							width={300}
-// 							height={300}
-// 						/>
-// 					</div>
-// 					<div className="relative w-3/4 p-8">
-// 						<div className="flex flex-row gap-8 items-center">
-// 							<p className="text-h8 uppercase text-secondary font-semibold tracking-wider ">
-// 								{article.keywords[0]}
-// 							</p>
-// 							<div className="rounded-full bg-black-25 w-[4px] h-[4px]" />
-// 						</div>
-// 						<h3 className="font-antic text-h5 text-primary font-semibold tracking-wider transition-all ease-in-out hover:underline">
-// 							{article.headline}
-// 						</h3>
-// 						<p className="text-black-50">{article.article[0]}</p>
-// 					</div>
-// 				</div>
-// 			</Link>
-// 		);
-// 	});
-// };
+const mappingSubCategoryArticles = (array) => {
+	return array.map((article, index) => {
+		return (
+			<Link
+				href={`/${article.category}/${article.sub_category}/${article._id}`}
+				key={index}>
+				<div className="flex flex-row gap-16 hover:bg-black-10 transition-all ease-in-out cursor-pointer">
+					<div className="relative w-1/4 h-[170px]">
+						<Image
+							src={article.images[0].image_url}
+							alt={article.images[0].image_alt}
+							width={300}
+							height={300}
+						/>
+					</div>
+					<div className="relative w-3/4 p-8">
+						<div className="flex flex-row gap-8 items-center">
+							<p className="text-h8 uppercase text-secondary font-semibold tracking-wider ">
+								{article.keywords[0]}
+							</p>
+							<div className="rounded-full bg-black-25 w-[4px] h-[4px]" />
+						</div>
+						<h3 className="font-antic text-h5 text-primary font-semibold tracking-wider transition-all ease-in-out hover:underline">
+							{article.headline}
+						</h3>
+						<p className="text-black-50">{article.article[0]}</p>
+					</div>
+				</div>
+			</Link>
+		);
+	});
+};
 
-// export default async function SubCategory({ params }) {
-// 	const subCategory = params.sub_category;
-// 	const artiles = await getData(subCategory);
+export default async function SubCategory({ params }) {
+	const subCategory = params.sub_category;
+	const artiles = await getData(subCategory);
 
-// 	return (
-// 		<main>
-// 			<div className="bg-black-5 p-64">
-// 				<h1 className="font-antic tracking-wider font-medium capitalize border-b-2 border-y-black-10">
-// 					{subCategory}
-// 				</h1>
-// 				<div className="flex flex-row gap-16 py-24">
-// 					<div className="flex flex-col gap-8 py-24">
-// 						{mappingSubCategoryArticles(artiles)}
-// 					</div>
-// 					<div className="w-1/4"></div>
-// 				</div>
-// 			</div>
-// 		</main>
-// 	);
-// }
+	return (
+		<main>
+			<div className="bg-black-5 p-64">
+				<h1 className="font-antic tracking-wider font-medium capitalize border-b-2 border-y-black-10">
+					{subCategory}
+				</h1>
+				<div className="flex flex-row gap-16 py-24">
+					<div className="flex flex-col gap-8 py-24">
+						{mappingSubCategoryArticles(artiles)}
+					</div>
+					<div className="w-1/4"></div>
+				</div>
+			</div>
+		</main>
+	);
+}
