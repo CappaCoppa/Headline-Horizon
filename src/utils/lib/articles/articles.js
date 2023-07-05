f70f25c52be94e73dcae39e730fa7a218690c74e
unknown <luk.vainius@gmail.com>
2023-07-05 22:11:57 +0100

    updated articles fetch method

--- src/utils/lib/articles/articles.js
+++ src/utils/lib/articles/articles.js
@@ -1,6 +1,8 @@
 exports.getArticles = async () => {
 	try {
-		const res = await fetch(`${process.env.API_URL}/api/articles`);
+		const res = await fetch(`${process.env.API_URL}/api/articles`, {
+			next: { revalidate: 240 },
+		});
 		return await res.json();
 	} catch (err) {
 		console.log(err);
