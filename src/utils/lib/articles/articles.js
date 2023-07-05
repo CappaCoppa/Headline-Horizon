f7a79924cf0c30442bf54d96e27d807e8c972af6
unknown <luk.vainius@gmail.com>
2023-07-05 21:19:01 +0100

    updated feed.xml and categories fetch function

--- src/utils/lib/articles/articles.js
+++ src/utils/lib/articles/articles.js
@@ -1,8 +1,6 @@
 exports.getArticles = async () => {
 	try {
-		const res = await fetch(`${process.env.API_URL}/api/articles`, {
-			cache: "force-cache",
-		});
+		const res = await fetch(`${process.env.API_URL}/api/articles`);
 		return await res.json();
 	} catch (err) {
 		console.log(err);
