78420ab266daded55678aeee954e0a24c6c49951
unknown <luk.vainius@gmail.com>
2023-07-09 08:45:57 +0100

    updated sitemap fetches

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,4 +1,4 @@
-import { getArticles } from "@/utils/lib/sitemap/articles/articles";
+import { getArticles } from "@/utils/lib/articles/articles";
 import { getCategories } from "@/utils/lib/sitemap/categories/categories";
 
 export default async function sitemap() {
