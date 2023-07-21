422e231fc1b8befcd2925af79b92463d4fa5c986
Lukas Lukas <luk.vainius@gmail.com>
2023-07-21 23:10:05 +0100

    cleaned around the code removed console.logs and added key values to maps and fixed connection func

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -2,59 +2,59 @@ import { getArticles } from "@/utils/lib/articles/articles";
 import { getCategories } from "@/utils/lib/categories/categories";
 
 export default async function sitemap() {
-	const categories = await getCategories();
-	const articles = await getArticles();
+    const categories = await getCategories();
+    const articles = await getArticles();
 
-	const allArticles = articles.map((article) => {
-		try {
-			const trimed = article.sub_category.trim();
-			return {
-				url: `${process.env.API_URL}/${article.category}/${encodeURIComponent(
-					trimed
-				)}/${article._id}`,
-				lastModified: article.date,
-			};
-		} catch (e) {
-			console.log(e);
-		}
-	});
+    const allArticles = articles.map((article) => {
+        try {
+            const trimed = article.sub_category.trim();
+            return {
+                url: `${process.env.API_URL}/${
+                    article.category
+                }/${encodeURIComponent(trimed)}/${article._id}`,
+                lastModified: article.date,
+            };
+        } catch (e) {
+            console.log(e);
+        }
+    });
 
-	const allCategories = categories.map((category) => {
-		try {
-			return {
-				url: `${process.env.API_URL}/${category.category.title}`,
-				lastModified: new Date(),
-			};
-		} catch (e) {
-			console.log(
-				"this is the error for allCategories <++++++++++++++++= " + e
-			);
-		}
-	});
+    const allCategories = categories.map((category) => {
+        try {
+            return {
+                url: `${process.env.API_URL}/${category.category.title}`,
+                lastModified: new Date(),
+            };
+        } catch (e) {
+            console.log(
+                "this is the error for allCategories <++++++++++++++++= " + e
+            );
+        }
+    });
 
-	const allSubCategories = categories.map((category) => {
-		try {
-			return category.sub_category.map((sub_category) => {
-				const trimed = sub_category.title.trim();
-				return {
-					url: `${process.env.API_URL}/${
-						category.category.title
-					}/${encodeURIComponent(trimed)}`,
-					lastModified: new Date(),
-				};
-			});
-		} catch (e) {
-			console.log(`this is the thired console.log for and error: ${e}`);
-		}
-	});
+    const allSubCategories = categories.map((category) => {
+        try {
+            return category.sub_category.map((sub_category) => {
+                const trimed = sub_category.title.trim();
+                return {
+                    url: `${process.env.API_URL}/${
+                        category.category.title
+                    }/${encodeURIComponent(trimed)}`,
+                    lastModified: new Date(),
+                };
+            });
+        } catch (e) {
+            console.log(`this is the thired console.log for and error: ${e}`);
+        }
+    });
 
-	return [
-		{
-			url: `${process.env.API_URL}`,
-			lastModified: new Date(),
-		},
-		...allCategories,
-		...allSubCategories.flat(1),
-		...allArticles,
-	];
+    return [
+        {
+            url: `${process.env.API_URL}`,
+            lastModified: new Date(),
+        },
+        ...allCategories,
+        ...allSubCategories.flat(1),
+        ...allArticles,
+    ];
 }
--- src/utils/db/connection.js
+++ src/utils/db/connection.js
@@ -1,24 +1,14 @@
 import { MongoClient } from "mongodb";
 
-let cachedClient = null;
-
 const db_connection = async () => {
-	try {
-		if (cachedClient && cachedClient.isConnected()) {
-			console.log("Using cached MongoDB client");
-			return cachedClient;
-		} else {
-			const client = await MongoClient.connect(process.env.MONGO_URI, {
-				poolSize: 10, // sets the number of connections in the pool default is 5
-			});
-			console.log("Connected to MongoDB");
-			cachedClient = client;
-			return client;
-		}
-	} catch (err) {
-		console.log(err);
-		return err;
-	}
+    try {
+        const client = await MongoClient.connect(process.env.MONGO_URI);
+        console.log("Connected to MongoDB");
+        return client;
+    } catch (err) {
+        console.log(err);
+        return err;
+    }
 };
 
 export default db_connection;
