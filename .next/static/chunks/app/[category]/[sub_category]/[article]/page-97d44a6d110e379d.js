f734c5927242cc29bcbcf5be471d1f5cff14b4d8
CappaCoppa <101198822+CappaCoppa@users.noreply.github.com>
2023-07-21 23:52:45 +0100

    Delete .next directory

--- .next/static/chunks/app/[category]/[sub_category]/[article]/page-97d44a6d110e379d.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[47],{7759:function(n,e,u){Promise.resolve().then(u.t.bind(u,5962,23))}},function(n){n.O(0,[962,253,769,744],function(){return n(n.s=7759)}),_N_E=n.O()}]);
--- .next/static/chunks/app/[category]/page-2e5c323770a4046f.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[862,931,539],{9292:function(e,n,s){Promise.resolve().then(s.t.bind(s,5962,23)),Promise.resolve().then(s.t.bind(s,7477,23))}},function(e){e.O(0,[477,962,253,769,744],function(){return e(e.s=9292)}),_N_E=e.O()}]);
