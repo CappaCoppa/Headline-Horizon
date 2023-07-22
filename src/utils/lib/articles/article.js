140466b907121c4e05c8b38035ff36cb94291c39
Lukas Lukas <luk.vainius@gmail.com>
2023-07-23 00:07:17 +0100

    just a test

--- src/utils/lib/articles/article.js
+++ src/utils/lib/articles/article.js
@@ -1,11 +1,14 @@
 exports.getArticle = async (category, sub_category, id) => {
-	try {
-		const res = await fetch(
-			`${process.env.API_URL}/api/articles/${category}/${sub_category}/${id}`,
-			{ cache: "force-cache" }
-		);
-		return await res.json();
-	} catch (err) {
-		console.log(`An error occured while fetching data from the server: ${err}`);
-	}
+    try {
+        const res = await fetch(
+            `${process.env.API_URL}/api/articles/${category}/${sub_category}/${id}`,
+            { cache: "force-cache" }
+        );
+        return await res.json();
+    } catch (err) {
+        console.log(
+            `An error occured while fetching data from the server: ${err}`
+        );
+        //comment
+    }
 };
