8dd6531f896e20dff9612a672140304ce5bdea6f
Lukas Lukas <luk.vainius@gmail.com>
2023-07-22 06:59:54 +0100

    added console.log of res

--- src/utils/lib/categories/categories.js
+++ src/utils/lib/categories/categories.js
@@ -1,10 +1,11 @@
 exports.getCategories = async () => {
-	try {
-		const res = await fetch(`${process.env.API_URL}/api/categories`, {
-			cache: "force-cache",
-		});
-		return await res.json();
-	} catch (err) {
-		console.log(err);
-	}
+    try {
+        const res = await fetch(`${process.env.API_URL}/api/categories`, {
+            cache: "force-cache",
+        });
+        console.log(res);
+        return await res.json();
+    } catch (err) {
+        console.log(err);
+    }
 };
