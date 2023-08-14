5a63489a4b644d08578cc14c06722a580b958c75
Lukas Lukas <luk.vainius@gmail.com>
2023-08-14 23:13:12 +0100

    added the adsence adds head tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -58,6 +58,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
+            <head>
+                <script
+                    async
+                    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3446792269165420"
+                    crossorigin="anonymous"
+                ></script>
+            </head>
             <body
                 className={`${NotoSerif.variable} ${NotoSans.variable} overflow-x-hidden w-[100%]`}
             >
