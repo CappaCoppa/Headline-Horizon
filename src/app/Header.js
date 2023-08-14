e70733b2ad9c65f2656e383b6e0acdd0e0ba07a7
Lukas Lukas <luk.vainius@gmail.com>
2023-08-14 22:02:22 +0100

    added missed alts to dekstop and mobile logos

--- src/app/Header.js
+++ src/app/Header.js
@@ -31,10 +31,20 @@ export const Header = () => {
             <div className="bg-primary px-16 md:px-32 md:py-8 flex justify-between gap-16 items-center">
                 <Link href={process.env.API_URL}>
                     <div className="  text-h2 cursor-pointer py-8 m-0  text-black-5 xl:hidden">
-                        <Image src={MobileH} width={80} height={80} />
+                        <Image
+                            src={MobileH}
+                            width={80}
+                            height={80}
+                            alt="mobile logo"
+                        />
                     </div>
                     <div className=" text-h2 cursor-pointer  py-8  text-black-5 hidden xl:block">
-                        <Image src={DesktopH} width={600} height={500} />
+                        <Image
+                            src={DesktopH}
+                            width={600}
+                            height={500}
+                            alt="desktop logo"
+                        />
                     </div>
                 </Link>
                 <div>
