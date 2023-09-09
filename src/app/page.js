0364732bfae04fbb68030a2c1214402bda6e9cce
Lukas Lukas <luk.vainius@gmail.com>
2023-09-09 23:01:59 +0100

    test

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,5 @@
 export default async function Home() {
     try {
-        const objectsArray = await getArticles();
-        const reversedArray = objectsArray.reverse();
         return <main>hello</main>;
     } catch (err) {
         console.log(err);
